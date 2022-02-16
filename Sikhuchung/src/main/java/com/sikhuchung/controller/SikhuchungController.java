94deee771680650c0d93a4742b5b94b90e0974cc
DESKTOP-KNSDT44\ADMIN <wkdvlfflq0104@hanmail.net>
2022-02-16 16:59:02 +0900

    스프링 부트 연동 테스트

--- Sikhuchung/src/main/java/com/sikhuchung/controller/SikhuchungController.java
+++ Sikhuchung/src/main/java/com/sikhuchung/controller/SikhuchungController.java
@@ -2,6 +2,7 @@ package com.sikhuchung.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import com.sikhuchung.service.SikhuchungService;
 
@@ -10,5 +11,11 @@ public class SikhuchungController {
 
     @Autowired
     private SikhuchungService sikhuchungService;
+
+    @GetMapping(value = "/sikhuchung/sikhuchungTest")
+    public String sikhuchungTest() {
+
+        return "sikhuchung/sikhuchungTest";
+    }
 
 }
