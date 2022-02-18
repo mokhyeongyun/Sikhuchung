7df77d42932b2c707446800d832f72f535640ec9
DESKTOP-VM8G4PM\mok <thajwm739@naver.com>
2022-02-18 17:27:57 +0900

    마이페이지- 주문목록, 회원정보변경비밀번호확인, 회원정보변경, 회원탈퇴 화면 컨트롤러 추가

--- Sikhuchung/src/main/java/com/sikhuchung/controller/SikhuchungController.java
+++ Sikhuchung/src/main/java/com/sikhuchung/controller/SikhuchungController.java
@@ -30,29 +30,52 @@ public class SikhuchungController {
         return "sikhuchung/noticelist";
     }
 
-    // 로그인 화면이동
+    // 로그인 화면이동 -- 현균
     @GetMapping(value = "/sikhuchung/login.do")
     public String login() {
         return "sikhuchung/login";
     }
 
-    // 회원가입 화면 이동
+    // 회원가입 화면 이동 -- 현균
     @GetMapping(value = "/sikhuchung/join.do")
     public String join() {
         return "sikhuchung/join";
     }
 
-    // 아이디찾기 화면 이동
+    // 아이디찾기 화면 이동 -- 현균
     @GetMapping(value = "/sikhuchung/find_id.do")
     public String findId() {
         return "sikhuchung/find_id";
     }
 
-    // 비밀번호찾기 화면 이동
+    // 비밀번호찾기 화면 이동 -- 현균
     @GetMapping(value = "/sikhuchung/find_pw.do")
     public String findPw() {
         return "sikhuchung/find_pw";
+    }
 
+    // 마이페이지-주문목록 화면 이동 -- 현균
+    @GetMapping(value = "/sikhuchung/mypageOrderInfo.do")
+    public String mypageOrderInfo() {
+        return "sikhuchung/mypageOrderInfo";
+    }
+
+    // 마이페이지-회원정보변경 비밀번호확인 화면 이동 -- 현균
+    @GetMapping(value = "/sikhuchung/mypageMemberInfoPwCheck.do")
+    public String mypageMemberInfoPwCheck() {
+        return "sikhuchung/mypageMemberInfoPwCheck";
+    }
+
+    // 마이페이지-회원정보변경 화면 이동 -- 현균
+    @GetMapping(value = "/sikhuchung/mypageMemberInfo.do")
+    public String mypageMemberInfo() {
+        return "sikhuchung/mypageMemberInfo";
+    }
+
+    // 마이페이지-회원탈퇴 화면 이동 -- 현균
+    @GetMapping(value = "/sikhuchung/mypageMemberQuit.do")
+    public String mypageMemberQuit() {
+        return "sikhuchung/mypageMemberQuit";
     }
 
 }
