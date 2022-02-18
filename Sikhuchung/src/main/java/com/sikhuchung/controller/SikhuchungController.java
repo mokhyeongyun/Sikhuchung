e9c019635b3a7191e45bcd6bbd27a51a2fd6b306
DESKTOP-84K04GJ\Y <yooj815@naver.com>
2022-02-18 14:34:30 +0900

    공지사항 출력

--- Sikhuchung/src/main/java/com/sikhuchung/controller/SikhuchungController.java
+++ Sikhuchung/src/main/java/com/sikhuchung/controller/SikhuchungController.java
@@ -1,9 +1,13 @@
 package com.sikhuchung.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.sikhuchung.domain.NoticeDTO;
 import com.sikhuchung.service.SikhuchungService;
 
 @Controller
@@ -16,6 +20,13 @@ public class SikhuchungController {
     public String sikhuchungTest() {
 
         return "sikhuchung/sikhuchungTest";
+    }
+
+    @GetMapping(value = "/sikhuchung/noticelist.do")
+    public String openNoticeList(Model model) {
+        List<NoticeDTO> noticeList = sikhuchungService.getNoticeList();
+        model.addAttribute("noticeList", noticeList);
+        return "sikhuchung/noticelist";
     }
 
 }
