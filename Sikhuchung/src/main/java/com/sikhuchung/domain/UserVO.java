c246b9fedf8edfb61e17d0023690d6838e53e496
DESKTOP-VM8G4PM\mok <thajwm739@naver.com>
2022-02-21 17:06:19 +0900

    링크 연결 및 userVO 생성

---
+++ Sikhuchung/src/main/java/com/sikhuchung/domain/UserVO.java
@@ -0,0 +1,18 @@
+package com.sikhuchung.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserVO {
+
+    private String uid;
+    private String upw;
+    private String uname;
+    private String uemail;
+    private int utel;
+    private int ugrade;
+    private String ujoindate;
+
+}
